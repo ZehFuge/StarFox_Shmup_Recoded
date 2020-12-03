4817a36d1d5d47453c095b78cc340d3c685aea2e
ZehFuge <72021556+ZehFuge@users.noreply.github.com>
2020-12-03 21:07:34 +0100

    Update game_settings.py

--- game/game_settings.py
+++ game/game_settings.py
@@ -9,27 +9,15 @@ import pygame
 
 # library init block
 pygame.init
-# pygame.mixer.pre_init(44100, -16, 50, 4096)
-# pygame.mixer.init()
-# pygame.font.init()
+pygame.mixer.pre_init(44100, -16, 50, 4096)
+pygame.mixer.init()
+pygame.font.init()
 
 
 # window information
 WIDTH = 1200
 HIGHT = 800
 screen = pygame.display.set_mode((WIDTH, HIGHT))
-
-
-# pre defined colors
-BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
-YELLOW = (255, 255, 0)
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
-BLUE = (0, 0, 255)
-CONVERT_SCORE = (89, 193, 53)
-CONVERT_WINGS = (172, 50, 50)
-CONVERT_PLAYER = (181, 230, 29)
 
 
 # save image and sound folder dir to var
@@ -39,24 +27,21 @@ snd_dir = path.join(path.dirname(__file__), "snd")
 
 # image loading block
 # load backgrounds
-backgrounds = {}
-backgrounds[0] = pygame.image.load(path.join(img_dir, "space_background_1200x800_nsm.png")).convert()
+backgrounds = {0: pygame.image.load(path.join(img_dir, "space_background_1200x800_nsm.png")).convert()}
 
 # load buttons
 # 0 = idle | 1 = mouseover
-buttons = {}
-buttons["play0"] = pygame.image.load(path.join(img_dir, "startmenu_play_button_rgb_red.png")).convert()
-buttons["play1"] = pygame.image.load(path.join(img_dir, "startmenu_play_button_mouseover_rgb_red.png")).convert()
+buttons = {"play0": pygame.image.load(path.join(img_dir, "startmenu_play_button_rgb_red.png")).convert_alpha(),
+           "play1": pygame.image.load(path.join(img_dir, "startmenu_play_button_mouseover_rgb_red.png")).convert_alpha()}
 
 # buttons set colorkey
 buttons["play0"].set_colorkey(RED)
 buttons["play1"].set_colorkey(RED)
 
 # load player sprites
-player_images = {}
-player_images[0] = pygame.image.load(path.join(img_dir, "arwing_idle.png")).convert_alpha()
-player_images[1] = pygame.image.load(path.join(img_dir, "arwing_left.png")).convert_alpha()
-player_images[2] = pygame.image.load(path.join(img_dir, "arwing_right.png")).convert_alpha()
+player_images = {0: pygame.image.load(path.join(img_dir, "arwing_idle.png")).convert_alpha(),
+                 1: pygame.image.load(path.join(img_dir, "arwing_left.png")).convert_alpha(),
+                 2: pygame.image.load(path.join(img_dir, "arwing_right.png")).convert_alpha()}
 
 # transform player sprites to right size
 # ...until I get my shit done making it in the right size as png
@@ -77,9 +62,11 @@ all_sprites.clear(screen, backgrounds[0])
 # layer predefinition block
 # create layer for an object
 #   self._layer = layers[classname]
-layers = {}
-layers["Buttons"] = 1
-layers["Mouse"] = 2
+layers = {"Buttons": 1,
+          "Mouse": 2}
+
+# sound loading block
+sounds = {"Buttons": pygame.mixer.Sound(path.join(snd_dir, "menu_mouseover_sfx.ogg"))}
 
 
 # class block
@@ -88,10 +75,13 @@ class Buttons(pygame.sprite.DirtySprite):
     def __init__(self, buttontype, x, y):
         # asign sprite to groups
         pygame.sprite.DirtySprite.__init__(self)
+
         # safe classname for events and handling
         self.classname = "Buttons"
+
         # needed for redrawing
         self.dirty = 1
+
         # set layer for draw order
         self._layer = layers[self.classname]
 
@@ -99,40 +89,56 @@ class Buttons(pygame.sprite.DirtySprite):
         all_sprites.add(self)
         all_buttons.add(self)
 
-        # needed to change between idle and mouseover button
-        self.image_counter = 0
-
         # set idle button image at first and get its rectangle
         self.buttontype = buttontype
-        self.image = buttons[self.buttontype + str(self.image_counter)]
+
+        # set vars for collision handling
+        self.collision = 0
+        self.collision_detector = 0
+
+        # load image and get its rect and mask
+        self.image = buttons[self.buttontype + str(self.collision)]
         self.rect = self.image.get_rect()
-        self.radius = self.rect.width/2
+        self.mask = pygame.mask.from_surface(self.image)
 
         # set rectangle to given position
         self.rect.x = x
         self.rect.y = y
 
-        # let sounds only play once
-        self.soundcontroller = 1
 
     def update(self):
-        # check collision
+        # keep mask updated
+        self.mask = pygame.mask.from_surface(self.image)
+
+        # check for collision
         self.check_collision()
 
-        # change image if needed
-        self.change_image()
+    # change self.image depending on collision
+    # also used to play mouseover sound
+    def change_image(self, hits):
+        # if there is a collision ...
+        if hits:
+            # ... check if image is already changed
+            if self.image != buttons[self.buttontype + str(1)]:
+                # if not, change it and set dirty
+                self.image = buttons[self.buttontype + str(1)]
+                sounds[self.classname].play()
+                self.dirty = 1
+        # if there is no collision
+        else:
+            # ... check if image is already changed
+            if self.image != buttons[self.buttontype + str(0)]:
+                # if not, change it and set dirty
+                self.image = buttons[self.buttontype + str(0)]
+                self.dirty = 1
 
     def check_collision(self):
-        hits = pygame.sprite.spritecollide(self, all_mouses, False)
+        # check if sprite rects collide
+        hits = pygame.sprite.groupcollide(all_mouses, all_buttons, False, False)
 
+        # if True, start a pixel perfect collision detection
         if hits:
-            if self.image_counter == 0:
-                self.image_counter = 1
-                self.dirty = 1
-        else:
-            if self.image_counter == 1:
-                self.image_counter = 0
-                self.dirty = 1
+            hits = pygame.sprite.groupcollide(all_mouses, all_buttons, False, False, pygame.sprite.collide_mask)
 
-    def change_image(self):
-        self.image = buttons[self.buttontype + str(self.image_counter)]
+        # set image changes if needed
+        self.change_image(hits)
