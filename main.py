83907f570bcec4abb95c5437d14272201ec655c8
ZehFuge <72021556+ZehFuge@users.noreply.github.com>
2020-12-05 21:17:12 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from sys import exit
 from game import *
 
 
+# define game handling ----------------------------------------------------------------------------------------------- #
 class Game():
     def __init__(self):
         # needed to display the right loop of the game class
@@ -15,14 +16,28 @@ class Game():
         # starting with layer = stage 1 for instant change
         self.layer = self.stage + 1
 
+        # create group for generated buttons objects
+        self.shown_buttons = {}
+
+        self.left_click = 0
 
 
-    # startmenu of the game
+    # startmenu of the game ------------------------------------------------------------------------------------------ #
     def menu(self):
-        # create button objects
-        play_button = GS.Buttons("play", 200, 600)
+        # set clear image and the surface
+        self.change_background_clear()
 
-        while 1:
+        # create button objects in screen order
+        play_button = GS.Buttons("play", 50, 600)
+        howto_button = GS.Buttons("howto", 450, 600)
+        exit_button = GS.Buttons("exit", 850, 600)
+
+        # save buttons in a list for easier kill and create
+        # buttons get killed in mouse method mouse.check_collision
+        self.shown_buttons = {play_button, howto_button, exit_button}
+
+        # handle menu until state gets changed by mouse.check_buttonpress while button collision
+        while self.state == "menu":
             # check input
             self.input_handler()
 
@@ -30,34 +45,93 @@ class Game():
             self.update_dirty_rects()
 
 
-    # check which loops need to be shown
+    # loop of the actual game ---------------------------------------------------------------------------------------- #
+    def play(self):
+        # set clear image and the surface
+        self.change_background_clear()
+
+        while self.state == "play":
+            # check input
+            self.input_handler()
+
+            # update by behavior, get changes and draw them
+            self.update_dirty_rects()
+
+
+    # handle how until state gets changed by mouse.check_buttonpress while button collision -------------------------- #
+    def howto(self):
+        # set clear image and the surface
+        self.change_background_clear()
+
+        # create button objects in screen order
+        menu_button = GS.Buttons("menu", 450, 600)
+
+        # save buttons in a list for easier kill and create
+        # buttons get killed in mouse method mouse.check_collision
+        self.shown_buttons = {menu_button}
+
+        while self.state == "howto":
+            # check input
+            self.input_handler()
+
+            # update all_sprites by behavior, get changes and draw them
+            self.update_dirty_rects()
+
+
+    # check which loops need to be shown ----------------------------------------------------------------------------- #
     def state_handler(self):
+        # main menu
         if self.state == "menu":
             self.menu()
 
-    # checks input while in any kind of menu
+        # actual game loop
+        if self.state == "play":
+            self.play()
+
+        # show instructions
+        if self.state == "howto":
+            self.howto()
+
+        # end game if exit was clicked
+        if self.state == "exit":
+            self.end_game()
+
+    # checks input while in any kind of menu ------------------------------------------------------------------------- #
     def input_handler(self):
         for event in GS.pygame.event.get():
+            # quit input --------------------------------------------------------------------------------------------- #
             if event.type == GS.pygame.QUIT:
                 self.end_game()
 
-            if event.type == GS.pygame.mouse.get_pressed():
-                self.mouse_pressed = GS.pygame.mouse.get_pressed()
 
-    # if event.type == QUIT
+            # keyboard inputs ---------------------------------------------------------------------------------------- #
+            if event.type == GS.pygame.KEYDOWN:
+                pass
+
+
+            # mouse inputs ------------------------------------------------------------------------------------------- #
+            if event.type == GS.pygame.MOUSEBUTTONDOWN:
+                if event.button == 1:
+                    self.left_click = 1
+            if event.type == GS.pygame.MOUSEBUTTONUP:
+                if event.button == 1:
+                    self.left_click = 0
+
+
+    # if event.type == QUIT ------------------------------------------------------------------------------------------ #
     def end_game(self):
         GS.pygame.quit()
         exit()
 
 
-    # update by behavior, get changes and draw them
+    # update by behavior, get changes and draw them ------------------------------------------------------------------ #
     def update_dirty_rects(self):
         GS.all_sprites.update()
         rects = GS.all_sprites.draw(GS.screen)
         GS.pygame.display.update(rects)
         GS.pygame.display.flip()
 
-    # change the background depend on self.stage re-rendering
+    # change the background depend on self.stage re-rendering -------------------------------------------------------- #
     def change_background_clear(self):
         GS.all_sprites.clear(GS.screen, GS.backgrounds[self.stage])
 
@@ -67,9 +141,21 @@ class Game():
 game = Game()
 
 
+# define mouse handling ---------------------------------------------------------------------------------------------- #
 class Mouse(GS.pygame.sprite.DirtySprite):
     def __init__(self):
         GS.pygame.sprite.DirtySprite.__init__(self)
+
+        """
+        # ------------------------------------------------------------------------------------------------------------ #
+        #                                                Usage notes:
+        #
+        # LEFT CLICK FROM MOUSE IS TAKE IN GAME CLASS:
+        # Left mouse button input is saved in the game class because of the lack of input check speed. In this class,
+        # every 20th-30th mouse click would have been taken.
+        # ------------------------------------------------------------------------------------------------------------ #
+        """
+
         # make mouse invisible, so only the image is shown
         GS.pygame.mouse.set_visible(False)
 
@@ -96,13 +182,13 @@ class Mouse(GS.pygame.sprite.DirtySprite):
         self.rect.centery = self.pos_y
 
 
-    # set objects behavior
+    # set objects behavior ------------------------------------------------------------------------------------------- #
     def update(self):
-        # keep mask updated
-        self.mask = GS.pygame.mask.from_surface(self.image)
-
         # get position of the mouse cursor
         self.get_position()
+
+        # check collision to change game.state
+        self.check_collision()
 
         # change mouse (x,y) depending on position
         self.sprite_repos()
@@ -110,23 +196,46 @@ class Mouse(GS.pygame.sprite.DirtySprite):
         # always draw the mouse sprite
         self.dirty = 1
 
-
+    # ---------------------------------------------------------------------------------------------------------------- #
     def get_position(self):
         # safe new position
         self.pos_x, self.pos_y = GS.pygame.mouse.get_pos()
 
+    # ---------------------------------------------------------------------------------------------------------------- #
     def sprite_repos(self):
         # set image rect.center x and y on mouse position
         self.rect.centerx = self.pos_x
         self.rect.centery = self.pos_y
 
+    # ---------------------------------------------------------------------------------------------------------------- #
+    def check_collision(self):
+        # check if sprite rects collide
+        hits = GS.pygame.sprite.spritecollideany(self, GS.all_buttons)
 
+        # if True, start a pixel perfect collision detection
+        if hits:
+            mask_hits = GS.pygame.sprite.spritecollideany(self, GS.all_buttons, GS.pygame.sprite.collide_mask)
+
+            # check if left click is done by user
+            if mask_hits and game.left_click == 1:
+                # reset left click to be only taken once
+                game.left_click = 0
+                # change game.state depending on the button
+                game.state = mask_hits.buttontype
+
+                # kill existing button for a dictonary clean for new buttons
+                for button in game.shown_buttons:
+                    button.kill()
+        else:
+            mask_hits = None
+
+
+# create mouse object before the main loop
 mouse = Mouse()
 
 
-# main loop
+# main loop ---------------------------------------------------------------------------------------------------------- #
 if __name__ == "__main__":
-# while game_running == 1:
-    GS.clock.tick(GS.FPS)
-    game.state_handler()
-
+    while game:
+        GS.clock.tick(GS.FPS)
+        game.state_handler()
