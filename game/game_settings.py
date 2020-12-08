ed9c0dffb70b53fc4364c56a2ebb2146217f4c12
ZehFuge <72021556+ZehFuge@users.noreply.github.com>
2020-12-08 10:47:36 +0100

    Update game_settings.py

--- game/game_settings.py
+++ game/game_settings.py
@@ -15,7 +15,7 @@ RED = (255, 0, 0)
 # window information ------------------------------------------------------------------------------------------------- #
 WIDTH = 1200
 HIGHT = 800
-screen = pygame.display.set_mode((WIDTH, HIGHT), pygame.FULLSCREEN)
+screen = pygame.display.set_mode((WIDTH, HIGHT))
 FPS = 60
 clock = pygame.time.Clock()
 
@@ -45,12 +45,12 @@ buttons = {"play0" : pygame.image.load(path.join(img_dir, "button_play.png")).co
 
 
 # load information/menu images
-info_imgs = {"howto0" : pygame.image.load(path.join(img_dir, "info_movement.png")).convert(),
+info_images = {"howto0" : pygame.image.load(path.join(img_dir, "info_movement.png")).convert(),
             "howto1" : pygame.image.load(path.join(img_dir, "info_powerups.png")).convert()}
 
 # set colorkey | remove after images are made transparent
-info_imgs["howto0"].set_colorkey(RED)
-info_imgs["howto1"].set_colorkey(RED)
+info_images["howto0"].set_colorkey(RED)
+info_images["howto1"].set_colorkey(RED)
 buttons["next0"].set_colorkey(RED)
 buttons["next1"].set_colorkey(RED)
 
@@ -62,7 +62,7 @@ player_images = {0: pygame.image.load(path.join(img_dir, "arwing_idle.png")).con
 
 # transform player sprites to right size
 # ...until I get my shit done making it in the right size as png
-for counter in range(3):
+for counter in player_images:
     player_images[counter] = pygame.transform.scale(player_images[counter], (96, 96))
 
 
@@ -70,6 +70,7 @@ for counter in range(3):
 all_buttons = pygame.sprite.Group()
 all_mouses = pygame.sprite.Group()
 all_infos = pygame.sprite.Group()
+all_players = pygame.sprite.Group()
 
 # create the all sprites group from here, to use all classes
 all_sprites = pygame.sprite.LayeredDirty()
@@ -79,9 +80,13 @@ all_sprites = pygame.sprite.LayeredDirty()
 # create layer for an object
 #   self._layer = layers[classname]
 layers = {"background_fx": 1,
-          "Info_Images": 2,
-          "Buttons": 3,
-          "Mouse": 4}
+          "Info_Images"  : 2,
+          "Buttons"      : 3,
+          "Mouse"        : 4,
+          "Player"       : 5,
+          "Enemy"        : 5,
+          "Meteor"       : 6,
+          "GUI"          : 7}
 
 
 # sound loading block ------------------------------------------------------------------------------------------------ #
@@ -92,7 +97,7 @@ sounds = {"Buttons": pygame.mixer.Sound(path.join(snd_dir, "menu_mouseover_sfx.o
 sounds["Buttons"].set_volume(0.5)
 
 
-# class block -------------------------------------------------------------------------------------------------------- #
+# game handling classes block ---------------------------------------------------------------------------------------- #
 # used to create buttons
 class Buttons(pygame.sprite.DirtySprite):
     def __init__(self, buttontype, x, y):
@@ -191,7 +196,7 @@ class Info_Images(pygame.sprite.DirtySprite):
 
         # load image and get its rect and mask
         self.gamestate = gamestate
-        self.image = info_imgs[self.gamestate + str(self.toggle)]
+        self.image = info_images[self.gamestate + str(self.toggle)]
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
@@ -208,8 +213,98 @@ class Info_Images(pygame.sprite.DirtySprite):
     # handling toggling by "next" button ----------------------------------------------------------------------------- #
     def check_toggle(self):
         if self.toggle != self.old_toggle:
-            self.image = info_imgs[self.gamestate + str(self.toggle)]
+            self.image = info_images[self.gamestate + str(self.toggle)]
             self.old_toggle = self.toggle
+
+
+# sprite classes block ----------------------------------------------------------------------------------------------- #
+class Player(pygame.sprite.DirtySprite):
+    def __init__(self, x, y):
+        pygame.sprite.DirtySprite.__init__(self)
+
+        # safe classname for events and handling
+        self.classname = "Player"
+
+        # needed for redrawing
+        self.dirty = 1
+
+        # set layer for draw order
+        self._layer = layers[self.classname]
+
+        # add to group after setting layer
+        all_sprites.add(self)
+        all_players.add(self)
+
+        # set var for movestatement
+        # 0 = idle | 1 = left | 2 = right
+        self.fly_direction = 0
+
+        # set image for object and get its rect and mask
+        self.image = player_images[self.fly_direction]
+        self.rect = self.image.get_rect()
+        self.mask = pygame.mask.from_surface(self.image)
+
+        # get start position
+        self.rect.x = x
+        self.rect.y = y
+
+        # declare movement speed of object
+        self.velocity = 2
+
+
+    def update(self):
+        # check input for movement update
+        self.movement_handler()
+
+
+    # get keyboard input and change position ------------------------------------------------------------------------- #
+    def movement_handler(self):
+        keystate = pygame.key.get_pressed()
+
+        # check if player moved (W, A, S, D)
+        if keystate[pygame.K_w]:
+            self.rect.y -= self.velocity
+            self.fly_direction = 0
+            self.dirty = 1
+        if keystate[pygame.K_a]:
+            self.rect.x -= self.velocity
+            self.fly_direction = 1
+            self.dirty = 1
+        if keystate[pygame.K_s]:
+            self.rect.y += self.velocity
+            self.fly_direction = 0
+            self.dirty = 1
+        if keystate[pygame.K_d]:
+            self.rect.x += self.velocity
+            self.fly_direction = 2
+            self.dirty = 1
+
+        # reset fly direction if player isn't moving left (1) or right (2) back to idle (0)
+        if not keystate[pygame.K_a] and not keystate[pygame.K_d]:
+            self.fly_direction = 0
+            self.dirty = 1
+
+        # change image depending on self.fly_direction
+        self.set_move_image()
+
+        # check if player wants to leave the screen
+        self.check_moverange()
+
+    # stops player to leave screen ----------------------------------------------------------------------------------- #
+    def check_moverange(self):
+        if self.rect.right > WIDTH:
+            self.rect.right = WIDTH
+        if self.rect.left < 0:
+            self.rect.left = 0
+        if self.rect.bottom > HIGHT:
+            self.rect.bottom = HIGHT
+        if self.rect.top < 0:
+            self.rect.top = 0
+
+    # update image by move_image counter and get new mask ------------------------------------------------------------ #
+    def set_move_image(self):
+        self.image = player_images[self.fly_direction]
+        self.mask = pygame.mask.from_surface(self.image)
 
 # test environment --------------------------------------------------------------------------------------------------- #
 if __name__ == "__main__":
